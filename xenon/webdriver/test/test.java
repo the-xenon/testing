e15b5160ec35d1b91183d7862e43f4bb985abeaa
xenon <xenon@inbox.ru>
2015-10-17 23:26:47 +0600

    first commit

---
+++ xenon/webdriver/test/test.java
@@ -0,0 +1,10 @@
+package xenon.webdriver.test;
+
+/**
+ * Created by xenon on 17.10.2015.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is test");
+    }
+}
