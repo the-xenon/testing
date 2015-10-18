8dddf2301f36ee98086d3d70bc7323aa193694eb
xenon <xenon@inbox.ru>
2015-10-19 01:11:06 +0600

    Logging to betariv, waiting for an element visibility issue

--- src/xenon/webdriver/test/BetaRivTest.java
+++ src/xenon/webdriver/test/BetaRivTest.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.security.UserAndPassword;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import xenon.webdriver.test.ITestStarter;
 
@@ -24,6 +27,14 @@ public class BetaRivTest implements ITestStarter {
         driver.get("http://beta:7sidQueplo@beta.ragazzeinvendita.com");
 
         checkAdultWarningDialogAndClose();
+
+        driver.findElement(By.id("loginLink")).click();
+        driver.findElement(By.name("login")).sendKeys("xenon22");
+        driver.findElement(By.name("pass")).sendKeys("password");
+        driver.findElement(By.id("lastField"));
+
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        //Wait<WebDriver> wait = new WebDriverWait(driver, 250);
 
         /*WebDriverWait wait = new WebDriverWait(driver, 20);
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
