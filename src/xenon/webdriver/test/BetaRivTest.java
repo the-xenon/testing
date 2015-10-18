882b12c83dc6e80ffb86ae6e2b85af34551f0c9f
xenon <xenon@inbox.ru>
2015-10-18 23:43:59 +0600

    checkAdultWarningDialogAndClose for betariv

--- src/xenon/webdriver/test/BetaRivTest.java
+++ src/xenon/webdriver/test/BetaRivTest.java
@@ -1,7 +1,9 @@
 package xenon.webdriver.test;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.security.UserAndPassword;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -13,14 +15,23 @@ import xenon.webdriver.test.ITestStarter;
  * Created by xenon on 10.10.2015.
  */
 public class BetaRivTest implements ITestStarter {
+    private WebDriver driver;
+
     @Override
     public void start() {
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         //driver.get("http://beta.ragazzeinvendita.com");
         driver.get("http://beta:7sidQueplo@beta.ragazzeinvendita.com");
+
+        checkAdultWarningDialogAndClose();
 
         /*WebDriverWait wait = new WebDriverWait(driver, 20);
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
         alert.authenticateUsing(new UserAndPassword("beta", "7sidQueplo"));*/
+    }
+
+    private void checkAdultWarningDialogAndClose() {
+        WebElement enterButton = driver.findElement(By.className("agree-button"));
+        enterButton.click();
     }
 }
