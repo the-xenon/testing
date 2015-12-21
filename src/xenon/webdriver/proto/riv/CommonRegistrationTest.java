e1adb382901d9757b73203871aeaa020373c1d5c
andrey625 <socialx@yandex.ru>
2015-12-21 19:07:35 +0600

    try to use testNG dataProvider for negative registration tests

--- src/xenon/webdriver/proto/riv/CommonRegistrationTest.java
+++ src/xenon/webdriver/proto/riv/CommonRegistrationTest.java
@@ -1,11 +1,17 @@
 package xenon.webdriver.proto.riv;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CommonRegistrationTest {
     PageHelper pageHelper = new PageHelper(DriverFactory.createDriver());
     UserHelper userHelper = new UserHelper();
+
+    final String MSG_SUCCESS_REGISTRATION = "Ciao! Ti diamo il benvenuto sul sito di webcam girls più amato dagli italiani. La tua iscrizione su RIV è avvenuta correttamente.";
+    final String MSG_LOGIN_SHORT = "Login: La lunghezza del campo deve essere: 3..25";
+    final String MSG_LOGIN_EMPTY = "Non hai compilato i seguenti campi: Login";
+    final String MSG_LOGIN_INVALID = "Login: Formato Campo non valido";
 
     //@Test
     public void positive() {
@@ -16,26 +22,38 @@ public class CommonRegistrationTest {
 
         Assert.assertTrue(loggedinPage.hasNicknameFieldWithNick(user.getNickname()));
         Assert.assertTrue(loggedinPage.hasCreditsField());
-        Assert.assertTrue(loggedinPage.hasNotificationFieldWithText(
-                "Ciao! Ti diamo il benvenuto sul sito di webcam girls più amato dagli italiani. La tua iscrizione su RIV è avvenuta correttamente."
-        ));
+        Assert.assertTrue(loggedinPage.hasNotificationFieldWithText(MSG_SUCCESS_REGISTRATION));
 
         loggedinPage.logout();
     }
-/*
-    @Test
-    public void negativeEmptyLogin() {
+
+    @DataProvider(name = "wrongLogin")
+    public Object[][] wrongLoginData() {
+        return new Object[][] {
+                {"", MSG_LOGIN_EMPTY},
+                {"a", MSG_LOGIN_SHORT}, {"gj", MSG_LOGIN_SHORT},
+                //5 digits in login
+                {"abcd82715efg", MSG_LOGIN_INVALID}, {"h8ijk27lmno15pr", MSG_LOGIN_INVALID},
+                //space in login
+                {"stuv wx", MSG_LOGIN_INVALID},
+                //invalid symbol in login
+                {"typical$user", MSG_LOGIN_INVALID}, {"invalid:symbol", MSG_LOGIN_INVALID}
+        };
+    }
+
+    @Test(dataProvider = "wrongLogin")
+    public void negativeWrongLogin(String login, String errorMessage) {
         CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
 
         RivUser user = userHelper.generateValidCommon();
-        user.setLogin("");
+        user.setLogin(login);
         RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
 
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Non hai compilato i seguenti campi: Login"
-        ));
+        Assert.assertTrue(
+                errorPage.hasErrorFieldWithText(errorMessage),
+                "Unable to find error message '" + errorMessage + "' for wrong login '" + login +"'.");
     }
-
+/*
     @Test
     public void negativeEmptyNick() {
         CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
@@ -62,17 +80,7 @@ public class CommonRegistrationTest {
         ));
     }
 
-    @Test
-    public void negativeEmptyPasswordConfirmation() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
 
-        RivUser user = userHelper.generateValidCommon();
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, "", true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Non hai compilato i seguenti campi: Conferma password"
-        ));
-    }
 
     @Test
     public void negativeEmptyEmail() {
@@ -84,6 +92,18 @@ public class CommonRegistrationTest {
 
         Assert.assertTrue(errorPage.hasErrorFieldWithText(
                 "Non hai compilato i seguenti campi: Email"
+        ));
+    }*/
+
+    @Test
+    public void negativeEmptyPasswordConfirmation() {
+        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
+
+        RivUser user = userHelper.generateValidCommon();
+        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, "", true);
+
+        Assert.assertTrue(errorPage.hasErrorFieldWithText(
+                "Non hai compilato i seguenti campi: Conferma password"
         ));
     }
 
@@ -135,72 +155,6 @@ public class CommonRegistrationTest {
 
         Assert.assertTrue(errorPage.hasErrorFieldWithText(
                 "Non hai compilato i seguenti campi: Confermo di aver letto ed accettato tutte i regolamenti"
-        ));
-    }*/
-
-
-    @Test
-    public void negative1SymbolLogin() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
-
-        RivUser user = userHelper.generateValidCommon();
-        user.setLogin("a");
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Login: La lunghezza del campo deve essere: 3..25"
-        ));
-    }
-
-    @Test
-    public void negative2SymbolsLogin() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
-
-        RivUser user = userHelper.generateValidCommon();
-        user.setLogin("GJ");
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Login: La lunghezza del campo deve essere: 3..25"
-        ));
-    }
-
-    @Test
-    public void negative4MoreDigitsInLogin() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
-
-        RivUser user = userHelper.generateValidCommon();
-        user.setLogin("test82715user");
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Login: Formato Campo non valido"
-        ));
-    }
-
-    @Test
-    public void negativeSpaceInLogin() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
-
-        RivUser user = userHelper.generateValidCommon();
-        user.setLogin("this is");
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Login: Formato Campo non valido"
-        ));
-    }
-
-    @Test
-    public void negativeWrongSymbolInLogin() {
-        CommonRegistrationPage regPage = pageHelper.navigateToCommonRegistrationPage();
-
-        RivUser user = userHelper.generateValidCommon();
-        user.setLogin("$wrongsymbol");
-        RegistrationErrorPage errorPage = regPage.incorrectRegistration(user, user.getPassword(), true);
-
-        Assert.assertTrue(errorPage.hasErrorFieldWithText(
-                "Login: Formato Campo non valido"
         ));
     }
 }
